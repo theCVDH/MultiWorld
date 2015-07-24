7ad746fb6f15466a6f8a2d0124ece5bee07cbdc0
Hunter Sharpe <huntersharpe@me.com>
2015-07-24 16:53:44 -0500

    Fixed ExceptionInInitialization Error

--- src/main/java/net/huntersharpe/multiworld/MultiWorld.java
+++ src/main/java/net/huntersharpe/multiworld/MultiWorld.java
@@ -27,8 +27,6 @@ import java.util.logging.Logger;
 @Plugin(id = "multiworld", name = "MultiWorld", version = "1.0")
 public class MultiWorld {
 
-    public double version = 1.1;
-
     @Inject
     private Logger logger;
 
@@ -55,18 +53,126 @@ public class MultiWorld {
         return instance;
     }
 
+    // /mw create <name> [type] [seed] Commad
+    CommandSpec createSeed = CommandSpec.builder()
+            .description(Texts.of("Create a world"))
+            .permission("multiword.use.create")
+            .executor(new Command())
+            .build();
+    CommandSpec createType = CommandSpec.builder()
+            .description(Texts.of("Create a world"))
+            .permission("multiword.use.create")
+            .executor(new Command())
+            .child(createSeed)
+            .build();
+    CommandSpec createName = CommandSpec.builder()
+            .description(Texts.of("Create a world"))
+            .permission("multiword.use.create")
+            .executor(new Command())
+            .child(createType)
+            .build();
+    CommandSpec create = CommandSpec.builder()
+            .description(Texts.of("Create a world"))
+            .permission("multiword.use.create")
+            .executor(new Command())
+            .child(createName)
+            .build();
+    // /mw delete <name> command
+    CommandSpec delName = CommandSpec.builder()
+            .description(Texts.of("Delete a world"))
+            .permission("multiword.use.delete")
+            .executor(new Command())
+            .build();
+    CommandSpec delete = CommandSpec.builder()
+            .description(Texts.of("Delete a world"))
+            .permission("multiword.use.delete")
+            .executor(new Command())
+            .child(delName)
+            .build();
+    // /mw help command
+    CommandSpec help = CommandSpec.builder()
+            .description(Texts.of("MultiWorld help Command"))
+            .permission("multiword.help")
+            .executor(new Command())
+            .build();
+    // /mw modify <type> <value> command
+    CommandSpec modValue = CommandSpec.builder()
+            .description(Texts.of("Modify your current world"))
+            .permission("multiword.modify")
+            .executor(new Command())
+            .build();
+    CommandSpec modType = CommandSpec.builder()
+            .description(Texts.of("Modify your current world"))
+            .permission("multiworld.modify")
+            .executor(new Command())
+            .child(modValue)
+            .build();
+    CommandSpec modify = CommandSpec.builder()
+            .description(Texts.of("Modify your current world"))
+            .permission("multiword.modify")
+            .executor(new Command())
+            .child(modType)
+            .build();
+    // /mw version command
+    CommandSpec version = CommandSpec.builder()
+            .description(Texts.of("MultiWorld version"))
+            .permission("multiword.version")
+            .executor(new Command())
+            .build();
+    // /tp <world> <x> <y> <z> command.
+    CommandSpec tpZ = CommandSpec.builder()
+            .description(Texts.of("Teleport to a world at an exact location"))
+            .permission("multiword.tp")
+            .executor(new Command())
+            .build();
+    CommandSpec tpY = CommandSpec.builder()
+            .description(Texts.of("Teleport to a world at an exact location"))
+            .permission("multiword.tp")
+            .executor(new Command())
+            .child(tpZ)
+            .build();
+    CommandSpec tpX = CommandSpec.builder()
+            .description(Texts.of("Teleport to a world at an exact location"))
+            .permission("multiword.tp")
+            .executor(new Command())
+            .child(tpY)
+            .build();
+    CommandSpec tpWorld = CommandSpec.builder()
+            .description(Texts.of("Teleport to a world at an exact location"))
+            .permission("multiword.tp")
+            .executor(new Command())
+            .child(tpX)
+            .build();
+    CommandSpec tp = CommandSpec.builder()
+            .description(Texts.of("Teleport to a world at an exact location"))
+            .permission("multiword.tp")
+            .executor(new Command())
+            .child(tpWorld)
+            .build();
+    // /join <world> command
+    CommandSpec joinWorld = CommandSpec.builder()
+            .description(Texts.of("Join a world at its spawn."))
+            .permission("multiword.join")
+            .executor(new Command())
+            .build();
+    CommandSpec join = CommandSpec.builder()
+            .description(Texts.of("Join a world at its spawn."))
+            .permission("multiword.join")
+            .executor(new Command())
+            .child(joinWorld)
+            .build();
     CommandSpec mwCommandSpec = CommandSpec.builder()
             .description(Texts.of("MultiWorld Command"))
             .permission("multiword.use")
             .executor(new Command())
             //TODO: Add all children
-            .child(CommandRegistry.getInstance().create, "create")
-            .child(CommandRegistry.getInstance().delete, "delete")
-            .child(CommandRegistry.getInstance().modify, "modify")
-            .child(CommandRegistry.getInstance().version, "version")
-            .child(CommandRegistry.getInstance().tp, "tp")
-            .child(CommandRegistry.getInstance().join, "join")
-            .child(CommandRegistry.getInstance().help, "help")
+            .child(create, "create")
+            .child(delete, "delete")
+            .child(modify, "modify")
+            .child(version, "version")
+            .child(tp, "tp")
+            .child(join, "join")
+            .child(help, "help")
             .build();
 
     @Subscribe
